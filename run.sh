2ffaaa872734f84999fa848af0bf3ee4aaf964ee
dr inż. Łukasz Dróżdż <lukasz.drozdz@polsl.pl>
2026-08-14 23:23:36 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 [ ! -d 'wyniki' ] && mkdir 'wyniki'
 [ ! -d 'obrazki' ] && mkdir 'obrazki'
 
-parallel -j 8 --colsep='\t' --arg-file=params.dat octave {1} {2} {3} {4} | tee ../wyniki/out.txt
+parallel -j 8 --colsep='\t' --arg-file=params.dat octave {1} {2} {3} {4} | tee wyniki/out.txt
